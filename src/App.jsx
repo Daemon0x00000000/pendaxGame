88f9dfa103f3bf0c292300bef7028520a0e412f4
Lucas Durbec <kd.daemon.kd@gmail.com>
2024-01-31 17:07:07 +0100

    fix des conneries de maxence

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {createContext, useState} from "react";
 import './App.css'
+import './styles/index.scss'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 // import GamePage from './components/Game'
 // import stats from './components/stats'
@@ -7,7 +8,9 @@ import Nav from './components/nav'
 import ErrorPage from './components/error'
 import HomePage from './components/home'
 import {GamePage} from "./pages/GamePage.jsx";
+import logo from './logo.png'
 
+export const StatsContext = createContext({})
 function App() {
     const [stats, setStats] = useState({
         scores: [
@@ -16,22 +19,22 @@ function App() {
     })
   return (
       <StatsContext.Provider value={[stats, setStats]}>
-        <img src={logo} alt="Pendax Game" style={{
-            display: 'block',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            width: '50%',
-            marginTop: '5rem'
-        }} />
-        <BrowserRouter>
-            <Nav/>
-            <Routes>
-                <Route path="/game" element={<GamePage />} />
-                <Route path="/home" element={<HomePage />} />
-                {/* <Route path="/stats" element={<StatsPage />} /> */}
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-        </BrowserRouter>
+          <img src={logo} alt="Pendax Game" style={{
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              width: '50%',
+              marginTop: '5rem'
+          }}/>
+          <BrowserRouter>
+              <Nav/>
+              <Routes>
+                  <Route path="/game" element={<GamePage/>}/>
+                  <Route path="/home" element={<HomePage/>}/>
+                  {/* <Route path="/stats" element={<StatsPage />} /> */}
+                  <Route path="*" element={<ErrorPage/>}/>
+              </Routes>
+          </BrowserRouter>
       </StatsContext.Provider>
   )
 }
